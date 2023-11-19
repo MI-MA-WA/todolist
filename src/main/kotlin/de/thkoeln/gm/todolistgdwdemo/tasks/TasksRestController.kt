2ee4d44f25f4d5adf53c299b5d1e7bbae68ca077
Hoai Viet Nguyen <viet.nguyen@th-koeln.de>
2023-11-19 01:48:12 +0100

    change TaskRESTController to TaskRestController

--- src/main/kotlin/de/thkoeln/gm/todolistgdwdemo/tasks/TasksRestController.kt
+++ src/main/kotlin/de/thkoeln/gm/todolistgdwdemo/tasks/TasksRestController.kt
@@ -10,7 +10,7 @@ import java.util.*
 
 @RestController
 @RequestMapping("/api/v1")
-class TasksRESTController(private val usersService: UsersService, private val tasksService: TasksService )
+class TasksRestController(private val usersService: UsersService, private val tasksService: TasksService )
 {
 
     @PostMapping("/users/{userId}/tasks")

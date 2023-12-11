ac1f439b8fb12267dcf0fd230758b34d0d8dce12
Hoai Viet Nguyen <viet.nguyen@th-koeln.de>
2023-12-11 08:55:47 +0100

    change UsersController to RestController and add example configuration for postgres and mysql

--- src/main/kotlin/de/thkoeln/gm/todolistgdwdemo/users/UsersRestController.kt
+++ src/main/kotlin/de/thkoeln/gm/todolistgdwdemo/users/UsersRestController.kt
@@ -1,20 +1,15 @@
 package de.thkoeln.gm.todolistgdwdemo.users
 
-import de.thkoeln.gm.todolistgdwdemo.holidays.Holiday
-import de.thkoeln.gm.todolistgdwdemo.holidays.HolidaysService
-import de.thkoeln.gm.todolistgdwdemo.tasks.Task
 import org.springframework.http.HttpStatus
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ResponseStatusException
 import java.util.*
 
 @RestController
-class UsersController (private val usersService: UsersService, private val holidaysService: HolidaysService) {
+class UsersRestController (private val usersService: UsersService) {
 
 
     @PostMapping("/users")
